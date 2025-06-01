56b2268da8ab0ca7b4d5acf06b832e76533e897f
Geet Trivedi <geettrivedi@Geets-MacBook-Pro.local>
2025-06-01 17:22:53 +0530

    housekeeping + bug fixes + ux

--- src/views/Premium.jsx
+++ src/views/Premium.jsx
@@ -1,25 +1,40 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleLoading } from "../utils/loaderSlice";
+import { addUser } from "../utils/userSlice";
 
 const Premium = () => {
 
     const dispatch = useDispatch();
+    const userData = useSelector(store => store.user);
     const [isPaymentDone, setIsPaymentDone] = useState(null);
+    const [isPremiumUser, setIsPremiumUser] = useState(null);
+
+    useEffect(() => {
+        dispatch(toggleLoading(true));
+        setIsPremiumUser(userData?.isPremium);
+        dispatch(toggleLoading(false));
+    }, [userData])
 
     const verifyPayment = async (orderId) => {
         dispatch(toggleLoading(true));
         const res = await axios.get(`${BASE_URL}/verifyPayment/${orderId}`, { withCredentials: true });
         setIsPaymentDone(res.data.isPaymentDone);
+
+        const user = await axios.get(`${BASE_URL}/profile/view`, { withCredentials: true });
+        dispatch(addUser(user.data));
+
         dispatch(toggleLoading(false));
     }
 
     const handleBuyAction = async (membershipType) => {
+        dispatch(toggleLoading(true));
         const _order = await axios.post(`${BASE_URL}/createOrder`, { membershipType }, { withCredentials: true });
         const { rzpKeyId: key, amount, currency, notes, orderId:order_id } = _order.data || {};
         const { firstName, lastName, emailId:email } = notes || {};
+        dispatch(toggleLoading(false));
         const options = {
             key, amount, currency, order_id, notes, 
             name: 'Coupler',
@@ -38,13 +53,13 @@ const Premium = () => {
         rzp.open();
     }
 
-    return isPaymentDone ? 
+    return (isPaymentDone || isPremiumUser) ? 
         <>
             <div className="h-dvh flex justify-center items-center text-center flex-col">
-                <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" className="h-[200px] w-auto" />
+                <img src={(isPremiumUser && !isPaymentDone) ? 'https://cdn-icons-png.flaticon.com/512/9967/9967681.png' : 'https://cdn-icons-png.flaticon.com/512/4436/4436481.png'} className="h-[200px] w-auto" />
                 <span className="mt-10 mb-30">
-                    <h3 className="text-xl font-bold mb-2">Payment Successful</h3>
-                    Congratulations, you are a premium user now!
+                    <h3 className="text-xl font-bold mb-2">{(isPremiumUser && !isPaymentDone) ? 'You are already a premium user!' : 'Payment Successful'}</h3>
+                    { isPaymentDone ? 'Congratulations, you are a premium user now!' : ''}
                 </span>
             </div>
         </> : <>
